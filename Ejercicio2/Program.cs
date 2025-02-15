2dfb38538914cd459ecb8fde5842230c41053471
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:28:09 -0600

    Ejerciccio 2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int num1 = 10;
+int num2 = 20;
+double numDouble = 2.5;
+char caracter = 'X';
+string texto = " es un carácter.";
+
+Console.WriteLine("Suma de los dos enteros: " + (num1 + num2));
+Console.WriteLine("Multiplicación del double por un int: " + (numDouble * num1));
+Console.WriteLine("Concatenación del char con un string: " + caracter + texto);
+
+Console.WriteLine("El Ing Daro es muy feo.");
+
+
+Console.WriteLine("El ejerecicio 2 pide que diga eso pero para mi el Ing Daro es una muy linda persona.");
